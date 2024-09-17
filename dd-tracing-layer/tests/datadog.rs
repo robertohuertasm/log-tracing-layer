0a9bdbb30656902d9d5c4cb454b0361ed8327f1f
Roberto Huertas <roberto.huertas@outlook.com>
2024-09-17 20:26:06 +0200

    testing env api key

--- dd-tracing-layer/tests/datadog.rs
+++ dd-tracing-layer/tests/datadog.rs
@@ -18,12 +18,22 @@ mod tests {
 
     #[test]
     fn datadog_works() {
+        println!(
+            "current directory {}",
+            std::env::current_dir().unwrap().display()
+        );
+        dotenvy::from_filename("/home/roberto/GIT/github/robertohuertasm/log-tracing-layer/.env")
+            .ok();
+
+        let api_key = std::env::var("DD-API-KEY").unwrap();
+        println!("api_key {}", api_key);
+
         let server = httpmock::MockServer::start();
         let _mock = server.mock(|when, then| {
             when.any_request();
             then.status(202).json_body(serde_json::json!([]));
         });
-        let options = DatadogOptions::new("dd-tracing-layer", "21695c1b35156511441c0d3ace5943f4")
+        let options = DatadogOptions::new("dd-tracing-layer", api_key)
             .with_url(server.base_url())
             .with_tags("env:dev");
         let dd = dd_tracing_layer::create(options);
